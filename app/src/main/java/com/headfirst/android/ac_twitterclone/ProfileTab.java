f3b0d37ba3dbde71df4afe041ea55886b34c8576
addxele <32433988+addxele@users.noreply.github.com>
2019-05-30 15:59:11 +0700

    Implemented User Tab

--- app/src/main/java/com/headfirst/android/ac_twitterclone/ProfileTab.java
+++ app/src/main/java/com/headfirst/android/ac_twitterclone/ProfileTab.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTab extends Fragment {
-
+    private EditText edtProfileName, edtBio, edtProfession, edtHobbies, edtFavoriteSport;
+    private Button btnUpdateInfo;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -24,7 +30,57 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtBio = view.findViewById(R.id.edtBio);
+        edtProfession = view.findViewById(R.id.edtProfession);
+        edtHobbies = view.findViewById(R.id.edtHobbies);
+        edtFavoriteSport = view.findViewById(R.id.edtFavoriteSport);
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        ParseUser appUser = ParseUser.getCurrentUser();
+
+        if (appUser.get("profileName") == null) {
+            edtProfileName.setText("");
+        }else{
+            edtProfileName.setText(appUser.get("profileName") +"");
+        }
+        if (appUser.get("bio") == null) {
+            edtBio.setText("");
+        }else {
+            edtBio.setText(appUser.get("bio") +"");
+        }
+        if (appUser.get("profession") == null) {
+            edtProfession.setText("");
+        }else {
+            edtProfession.setText(appUser.get("profession") + "");
+        }
+        if (appUser.get("hobbies") == null) {
+            edtHobbies.setText("");
+        }else {
+            edtHobbies.setText(appUser.get("hobbies") + "");
+        }
+        if (appUser.get("favoriteSport") == null) {
+            edtFavoriteSport.setText("");
+        }else {
+            edtFavoriteSport.setText(appUser.get("favoriteSport") + "");
+        }
+
+        btnUpdateInfo.setOnClickListener(v -> {
+            appUser.put("profileName", edtProfileName.getText().toString());
+            appUser.put("bio", edtBio.getText().toString());
+            appUser.put("profession", edtProfession.getText().toString());
+            appUser.put("hobbies", edtHobbies.getText().toString());
+            appUser.put("favoriteSport", edtFavoriteSport.getText().toString());
+            appUser.saveInBackground((e) ->{
+                if (e == null) {
+                    FancyToast.makeText(getContext(), "info updated successfully", FancyToast.LENGTH_LONG, FancyToast.INFO, true).show();
+                }else {
+                    FancyToast.makeText(getContext(), "info update failed", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                }
+            });
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/headfirst/android/ac_twitterclone/UserTab.java
+++ app/src/main/java/com/headfirst/android/ac_twitterclone/UserTab.java
@@ -5,15 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserTab extends Fragment {
-
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
     public UserTab() {
         // Required empty public constructor
@@ -24,7 +34,30 @@ public class UserTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_tab, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+        TextView txtLoading = view.findViewById(R.id.txtLoading);
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground((users, e) -> {
+            if (e == null) {
+                if (users.size() > 0) {
+                    for (ParseUser parseUser : users) {
+                        arrayList.add(parseUser.getUsername());
+                    }
+                    listView.setAdapter(arrayAdapter);
+                    txtLoading.animate().alpha(0).setDuration(1000);
+                    listView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        return view;
     }
 
 }
